5c3266edd692961c4eb53bf4c97ea86aa0173216
Stephen F <stephen@farver.io>
2018-05-27 22:53:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  output = ""
+  if array.length > 2
+    output = ""
+    array.each_with_index do |el, i|
+      if el
+  elsif array.length == 2 
+    puts "#{array[0]} and #{array[1]}"
+  elsif array.length == 1
+    puts array.first
+  end
 end
